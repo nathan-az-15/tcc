eb1c2dfbe19e928b03cad3363fd7b03610f2f0cf
nathan-az-15 <nata.mili@hotmail.com>
2020-10-25 15:29:14 -0300

    Update conecta.php

--- php/conecta.php
+++ php/conecta.php
@@ -1,5 +1,5 @@
 <?php
-$conexao = mysqli_connect('127.0.0.1','root','','bd_reo_tcc');
+$conexao = mysqli_connect(127.0.0.1,'root','','bd_reo_tcc');
 mysqli_set_charset($conexao, 'utf8');
 if($conexao->connect_error){die("Falha ao realizar a conexão" . $conexao->connect_error);}
 ?>
