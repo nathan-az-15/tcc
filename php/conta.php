e94644b7dd5e2b7bdb67bfe72355d29b97aa0f1d
nathan-az-15 <nata.mili@hotmail.com>
2020-11-03 15:23:35 -0300

    automatização conta

--- php/conta.php
+++ php/conta.php
@@ -43,10 +43,73 @@
       <a href="#"><i class="fas fa-user-alt"></i><span>Conta</span></a>
       </div>
     </div>
-    mobile navigation bar end-->
+    mobile navigation bar end--> 
+	
+<?php
+include 'conecta.php';
+session_start();
+echo "<meta charset='utf-8'>";
+$email=$_SESSION['email_usuario'];
+$conexao = mysqli_connect('127.0.0.1','root','','bd_reo_tcc') or die("erro de conexão");
 
-    <!--sidebar start-->
-    <div class="menu">
+if($email=="ADM_reforc@gmail.com"){
+  $consulta="SELECT * FROM administrador WHERE email_adm = '$email'";
+  $res = mysqli_query($conexao, $consulta);  //Executa o comando SQL, no caso para pegar todos os dados e retornar o valor da consulta em uma variavel ($res)  
+
+        $mostrar = mysqli_fetch_array($res);
+
+?>
+<div class="menu">
+      <center>
+      <h4>MENU</h4>
+      </center>
+      <a href="../inicio_adm.html"><i class="fas fa-home"></i><span>Início</span></a>
+      <a href="includeconteudos.php"><i class="fas fa-book-open"></i><span>Cadastro de conteúdos</span></a>
+      <a href="includequestoes.php"><i class="fas fa-file-alt"></i><span>Cadastro de questões</span></a>
+      <a href="includeindicacoes.php"><i class="fas fa-film"></i><span>Cadastro de indicações</span></a>
+      <a href="conta.php"><i class="fas fa-user-alt"></i><span>Conta</span></a>
+    </div>
+    <!--sidebar end-->
+
+    <div class="conteudo">
+
+<form action="editar_adm.php" method="POST">
+					
+						<h2> Dados Pessoais </h2>
+						
+						<p> Atualizar dados </p>
+						
+					   
+					    <label for="exampleInputEmail1">Nome</label>
+						<input type="TEXT" class="form-control1" id="exampleInputEmail1" aria-describedby="emailHelp" name="Nome" placeholder="Nome"  style="text-align: left " value="<?php echo utf8_encode($mostrar['nome_adm']);?>" required>
+					    				    
+					    <label for="exampleInputEmail1">Email</label>
+						<input type="email" class="form-control4" id="exampleInputEmail1" aria-describedby="emailHelp" name="Email" placeholder="Email" style="text-align: left" value="<?php echo utf8_encode($mostrar['email_adm']);?>" disabled>
+					    					   
+					    <button type="submit" class="btn2 btn-color2" style= "text-align:left">Salvar</button>
+						
+					   <br><br><br>
+						<p> Atualizar senha </p>
+                        <label for="exampleInputPassword1">Senha</label>
+                        <input type="password" class="form-control6" id="exampleInputPassword1" name="Senha"  placeholder="Senha" style="text-align: left" value="<?php echo $mostrar['senha_adm'];?>" required>
+                       
+					
+			
+			        <button type="submit" class="btn2 btn-color2" style= "text-align:left">Salvar</button>
+						
+					</form>
+<br>
+<br>
+<br>
+		<?php
+}
+else{
+  $consulta="SELECT * FROM usuario WHERE email_usuario = '$email'";
+$res = mysqli_query($conexao, $consulta);  //Executa o comando SQL, no caso para pegar todos os dados e retornar o valor da consulta em uma variavel ($res)  
+
+        $mostrar = mysqli_fetch_array($res);
+?>
+<div class="menu">
       <center>
       <h4>MENU</h4>
       </center>
@@ -60,19 +123,6 @@
     <!--sidebar end-->
 
     <div class="conteudo">
-	
-<?php
-include'conecta.php';
-session_start();
-echo "<meta charset='utf-8'>";
-$email=$_SESSION['email_usuario'];
-$conexao = mysqli_connect('127.0.0.1','root','','bd_reo_tcc') or die("erro de conexão");
-$consulta="SELECT * FROM usuario WHERE email_usuario = '$email'";
-$res = mysqli_query($conexao, $consulta);  //Executa o comando SQL, no caso para pegar todos os dados e retornar o valor da consulta em uma variavel ($res)  
-
-        $mostrar = mysqli_fetch_array($res);
-?>
-
 <form action="editar.php" method="POST">
 					
 						<h2> Dados Pessoais </h2>
@@ -117,7 +167,7 @@ $res = mysqli_query($conexao, $consulta);  //Executa o comando SQL, no caso para
 <br>
 		<?php
        echo "<a href='excluir.php?email=".$mostrar['email_usuario']."'><button class='btn1 btn-color1'>Excluir conta</button></a>";
-
+}
 ?>
 </div>
  
--- php/editar_adm.php
+++ php/editar_adm.php
@@ -1,15 +1,12 @@
 <?php
 
 $Nome=$_POST["Nome"];
-$Data_nasc=$_POST["data_nasc"];
-$Escolaridade=$_POST["escolaridade"];
 $Email=$_POST["Email"];
 $Senha=$_POST["Senha"];
-$Serie_ano=$_POST["Serie_ano"];
 
 $base=mysqli_connect('127.0.0.1', 'root','','bd_reo_tcc') or die ("erro de conexão");
 //update tabela set coluna=valor
-$regra="UPDATE usuario SET nome_usuario='$Nome', data_nasc='$Data_nasc', escolaridade='$Escolaridade', email_usuario='$Email', senha_usuario='$Senha', serie_ano='$Serie_ano'WHERE email_usuario='$Email'";
+$regra="UPDATE administrador SET nome_adm='$Nome', email_adm='$Email', senha_adm='$Senha' WHERE email_adm='$Email'";
 
 $res=mysqli_query($base,$regra) or die ("deu errado");
 	echo "<meta http-equiv='refresh' content='0;url=http://127.0.0.1/edsa-TCC_github/php/conta.php' />";
