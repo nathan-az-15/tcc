a901a6b77afc4d8fc3342333550210af4d2ae268
serafimaria <mariaeduardaserafim78@gmail.com>
2020-11-23 13:45:47 -0300

    titulo indicações

--- php/exibir_indicacoes.php
+++ php/exibir_indicacoes.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>Reforço Escolar Online | R.E.O</title>
+	<title>Reforço+ | Reforço Escolar Online</title>
 	<link rel="stylesheet" href="../css/inicio.css">
 	<link rel="stylesheet" href="../css/exibirindicacao.css">
 	<script src="https://kit.fontawesome.com/a68f3df9e0.js" crossorigin="anonymous"></script>
