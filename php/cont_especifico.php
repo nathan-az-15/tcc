b345edc29a4c685e59e0061b1c0a357a3ac0f30d
serafimaria <mariaeduardaserafim78@gmail.com>
2020-11-24 14:41:06 -0300

    design conteudo específico

--- php/cont_especifico.php
+++ php/cont_especifico.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="utf-8">
-    <title>Conteúdos | Reforço+</title>
-    <link rel="stylesheet" href="../css/inicio.css">
-    <link rel="stylesheet" type="text/css" href="../css/cont_especifico.css">
+  <meta charset="utf-8">
+  <title>Conteúdos | Reforço+</title>
+  <link rel="stylesheet" href="../css/inicio.css">
+  <link rel="stylesheet" type="text/css" href="../css/cont_especifico.css">
 
-    <script src="https://kit.fontawesome.com/a68f3df9e0.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a68f3df9e0.js" crossorigin="anonymous"></script>
 
-  </head>
-  <body>
-    <input type="checkbox" id="check">
-    <!--header area start-->
+</head>
 
-
-      <header>
-
-      <a href="../inicio.html"><img style="float:left; margin-left:52px; margin-top: 0px; margin-bottom: 1px; padding-top: 0px; border: 0;" src="../imagens/inicial/logo3.png"> </a>
-      <label for="check">
-        <i class="fas fa-align-justify" id="menu_btn"></i>
-      </label>
-      <div class="direita">
-        <a href="sair.php" class="sair">Sair</a>
-      </div>
-
-      </header>
-    <!--header area end-->
+<body>
+  <input type="checkbox" id="check">
+  <!--header area start-->
 
 
-    <!--mobile navigation bar start
+  <header>
+
+    <a href="../inicio.html"><img style="float:left; margin-left:52px; margin-top: 0px; margin-bottom: 1px; padding-top: 0px; border: 0;" src="../imagens/inicial/logo3.png"> </a>
+    <label for="check">
+      <i class="fas fa-align-justify" id="menu_btn"></i>
+    </label>
+    <div class="direita">
+      <a href="sair.php" class="sair">Sair</a>
+    </div>
+
+  </header>
+  <!--header area end-->
+
+
+  <!--mobile navigation bar start
     <div class="mobile_nav">
       <div class="nav_bar">
 
@@ -44,65 +46,62 @@
     </div>
     mobile navigation bar end-->
 
-    <!--sidebar start-->
-    <div class="menu">
-      <center>
+  <!--sidebar start-->
+  <div class="menu">
+    <center>
       <h4>MENU</h4>
-      </center>
-      <a href="../inicio.html"><i class="fas fa-home"></i><span>Início</span></a>
-      <a href="conteudos.php"><i class="fas fa-book-open"></i><span>Conteúdos</span></a>
-      <a href="testeniveis.php"><i class="fas fa-file-alt"></i><span>Testes de Nível</span></a>
-      <a href="#"><i class="fas fa-chart-line"></i><span>Desempenho</span></a>
-      <a href="indicacoes.php"><i class="fas fa-film"></i><span>Indicações</span></a>
-      <a href="conta.php"><i class="fas fa-user-alt"></i><span>Conta</span></a>
-    </div>
-    <!--sidebar end-->
-    <div class="conteudo">
-        <?php
-        include 'conecta.php';
-            $conn = mysqli_connect('127.0.0.1', 'root', '') or die("Não foi possível a conexão com o Banco");
-            $db = mysqli_select_db($conn,'bd_reo_tcc') or die("Não foi possível selecionar o Banco");
+    </center>
+    <a href="../inicio.html"><i class="fas fa-home"></i><span>Início</span></a>
+    <a href="conteudos.php"><i class="fas fa-book-open"></i><span>Conteúdos</span></a>
+    <a href="testeniveis.php"><i class="fas fa-file-alt"></i><span>Testes de Nível</span></a>
+    <a href="#"><i class="fas fa-chart-line"></i><span>Desempenho</span></a>
+    <a href="indicacoes.php"><i class="fas fa-film"></i><span>Indicações</span></a>
+    <a href="conta.php"><i class="fas fa-user-alt"></i><span>Conta</span></a>
+  </div>
+  <!--sidebar end-->
+  <div class="conteudo">
+    <?php
+    include 'conecta.php';
+    $conn = mysqli_connect('127.0.0.1', 'root', '') or die("Não foi possível a conexão com o Banco");
+    $db = mysqli_select_db($conn, 'bd_reo_tcc') or die("Não foi possível selecionar o Banco");
 
-            $cod_assunto = $_GET['assunto'];
+    $cod_assunto = $_GET['assunto'];
 
-            $sql = "SELECT * FROM conteudos where ID_cont like '".$cod_assunto."%'";
-            $cod = mysqli_query($conn,$sql);
+    $sql = "SELECT * FROM conteudos where ID_cont like '" . $cod_assunto . "%'";
+    $cod = mysqli_query($conn, $sql);
 
-            
 
-            $numRegistros = mysqli_num_rows($cod);
-            if ($numRegistros != 0) {
-              echo "<section class='flexbox'>";
-              while ($text = mysqli_fetch_object($cod)) {
-                $texto=$text->texto;
-                $texto=utf8_encode($texto);
-                $assunto=$text->assunto;
-                $assunto=utf8_encode($assunto);
-                   echo "<center><h1>".$assunto."</h1>
-                   <br><br><div class='texto'>".nl2br($texto).
 
-                   "</div><br><br>";
-                   $consulta_imagem = "SELECT * FROM imagens WHERE ID_cont LIKE '$cod_assunto'";
-                      $con_imagem = mysqli_query($conexao, $consulta_imagem);
-                      
-                      
-                   
-                     
-                    while ($mostrar_imagem = mysqli_fetch_object($con_imagem)) {
-                      
-                        echo "<center><img src='../imagens/imgconteudo/" . $mostrar_imagem->end_imagem  . "'></center><br><br>";
-                    
-                }
-                }
-                echo "</center></section>";
-              }
-            else{
-                echo "Nenhum registro";
-            }
+    $numRegistros = mysqli_num_rows($cod);
+    if ($numRegistros != 0) {
+      echo "<div class='global'>";
 
-        ?>
- </div>
-   <!--<script type="text/javascript">
+      while ($text = mysqli_fetch_object($cod)) {
+        $texto = $text->texto;
+        $texto = utf8_encode($texto);
+        $assunto = $text->assunto;
+        $assunto = utf8_encode($assunto);
+
+        echo "<center><h1>" . $assunto . "</h1>";
+        echo "<p>" . nl2br($texto) . "<br><br><br>";
+
+        $consulta_imagem = "SELECT * FROM imagens WHERE ID_cont LIKE '$cod_assunto'";
+        $con_imagem = mysqli_query($conexao, $consulta_imagem);
+
+        while ($mostrar_imagem = mysqli_fetch_object($con_imagem)) {
+
+          echo "<center><img src='../imagens/imgconteudo/" . $mostrar_imagem->end_imagem  . "'></center></p><br><br><br>";
+        }
+      }
+
+      echo "</div>";
+    } else {
+      echo "Nenhum registro";
+    }
+
+    ?>
+  </div>
+  <!--<script type="text/javascript">
     $(document).ready(function(){
       $('.nav_btn').click(function(){
         $('.mobile_nav_items').toggleClass('active');
@@ -111,4 +110,5 @@
     </script>-->
 
 </body>
+
 </html>
