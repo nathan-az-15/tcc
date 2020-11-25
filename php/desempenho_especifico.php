cdc660ba24093be1dc0b0506e00bba039328bb16
MiniBadasBR <matheusdellacrocce@hotmail.com>
2020-11-25 01:47:37 -0300

    Update desempenho_especifico.php

--- php/desempenho_especifico.php
+++ php/desempenho_especifico.php
@@ -1,3 +1,41 @@
+<style>/* CSS interno para as cores e tamanho do Objeto*/
+.cor0{ /* 0 acertos */
+  background-color: #000000;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 0px;
+}
+.cor1{ /* 1 acertos */
+  background-color: #ff2200;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 100px;
+}
+.cor2{ /* 2 acertos */
+  background-color: #ff6600;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 200px;
+}
+.cor3{ /* 3 acertos */
+  background-color: #fffb00;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 300px;
+}
+.cor4{ /* 4 acertos */
+  background-color: #1eff00;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 400px;
+}
+.cor5{ /* 5 acertos */
+  background-color: #00d9ff;
+  border: solid 1.5px #000000;
+  height: 1%;
+  width: 500px;
+}
+</style>
 <?php
 include 'conecta.php';
 
@@ -13,6 +51,7 @@ $AcertosTotais = 0;
 $FaceisTotais = 0;
 $MediasTotais = 0;
 $DificeisTotais = 0;
+$cor = "";
 
 $consultaUsu0 = "SELECT * FROM usuario WHERE email_usuario LIKE '$email_sessao'";
 $conUsu0 = mysqli_query($conexao, $consultaUsu0);
@@ -59,7 +98,7 @@ $conMat = mysqli_query($conexao, $consultaMat);
     echo "Acertos Totais: $AcertosTotais<br>";
     echo "Fáceis: $FaceisTotais<br>";
     echo "Médias: $MediasTotais<br>";
-    echo "Díficeis: $DificeisTotais<br><br>";
+    echo "Díficeis: $DificeisTotais<br><br><br>";
   }
 
 //--------------------------------------------------------------------SELECT do Conteúdo da matéria
@@ -68,40 +107,49 @@ $consultaCont = "SELECT * FROM conteudos WHERE cod_mat LIKE '$mat'";
 $conCont = mysqli_query($conexao, $consultaCont);
     
   while ($mostrarCont = mysqli_fetch_array($conCont)) {
-    echo "".$mostrarCont['assunto']."<br>";
+    echo "".$mostrarCont['assunto']." | " . $mostrarCont['ano'] . "º Ano <br>";
 
     $cont = $mostrarCont['ID_cont'];
 
     $consultaResp = "SELECT * FROM responde WHERE ID_usuario LIKE '$ID_usuario' AND id_cont LIKE '$cont'";
     $conResp = mysqli_query($conexao, $consultaResp); 
-  
+    
       while ($mostrarResp = mysqli_fetch_array($conResp)) {
+
+        if($mostrarResp['acertos'] == "0"){ //------------------------Cor para 0 acertos
+          $cor = "cor0";
+        }
+
+        if($mostrarResp['acertos'] == "1"){ //------------------------Cor para 1 acerto
+          $cor = "cor1";
+        }
+
+        if($mostrarResp['acertos'] == "2"){ //------------------------Cor para 2 acertos
+          $cor = "cor2";
+        }
+
+        if($mostrarResp['acertos'] == "3"){ //------------------------Cor para 3 acertos
+          $cor = "cor3";
+        }
+
+        if($mostrarResp['acertos'] == "4"){ //------------------------Cor para 4 acertos
+          $cor = "cor4";
+        }
+
+        if($mostrarResp['acertos'] == "5"){ //------------------------Cor para 5 acertos
+          $cor = "cor5";
+        }
+
+
         echo "Acertos:".$mostrarResp['acertos']."/5<br>";
-        $ColunaAcertos = $mostrarResp['acertos'] * 10;
-        echo "<input type='none' readonly='true' size='$ColunaAcertos'><br>";
+        echo "<input class='$cor' type='none' readonly='true'><br>"; //------------------------Objeto usado como linha do gráfico
 
         echo "Fáceis:".$mostrarResp['faceis']."<br>";
-        $ColunaFaceis = $mostrarResp['faceis'] * 10;
-        echo "<input type='none' readonly='true' size='$ColunaFaceis'><br>";
 
         echo "Médias:".$mostrarResp['medias']."<br>";
-        $ColunaMedias = $mostrarResp['medias'] * 10;
-        echo "<input type='none' readonly='true' size='$ColunaMedias'><br>";
 
-        echo "Difíceis:".$mostrarResp['dificeis']."<br>";
-        $ColunaDificeis = $mostrarResp['dificeis'] * 10;
-        echo "<input type='none' readonly='true' size='$ColunaDificeis'><br><br>";
+        echo "Difíceis:".$mostrarResp['dificeis']."<br><br>";
       }
     
   }
-
-
-
-
-
-
-
-
-
-
 ?>
